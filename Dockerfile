1b6add12b5fc146ae238aac46da4e5acc022ebec
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-05-05 07:29:12 +0800

    change to offical cloudreve image

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,11 @@
-FROM redis:alpine
+FROM cloudreve/cloudreve:latest
 
 COPY ./content /workdir/
 
 ARG MODE=build
 
-RUN apk add --no-cache runit curl gcompat tzdata \
+RUN apk add --no-cache runit curl aria2 redis \
     && chmod +x /workdir/*.sh /workdir/service/*/run \
-    && apk add --no-cache --virtual .build-deps jq \
-    && /workdir/install.sh \
-    && apk del .build-deps \
     && ln -s /workdir/service/* /etc/service/
 
 ENTRYPOINT ["runsvdir","-P","/etc/service"]
--- content/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Install aria2
-curl -s --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/P3TERX/Aria2-Pro-Core/releases/download/1.36.0_2021.08.22/aria2-1.36.0-static-linux-amd64.tar.gz -o - | tar -zxf - -C /usr/bin
-
-# Install Cloudreve
-VERSION="$(curl --retry 10 --retry-max-time 60 https://api.github.com/repos/cloudreve/Cloudreve/releases/latest | jq .tag_name | sed 's/\"//g')"
-curl -s --retry 10 --retry-max-time 20 -H "Cache-Control: no-cache" -fsSL github.com/cloudreve/Cloudreve/releases/download/${VERSION}/cloudreve_${VERSION}_linux_amd64.tar.gz -o - | tar -zxf - -C /usr/bin
