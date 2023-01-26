139d54eeecd09ab979477b48d7b63538971c4ed5
wy580477 <98247050+wy580477@users.noreply.github.com>
2023-01-26 13:20:29 +0800

    Update install.sh

--- content/install.sh
+++ content/install.sh
@@ -9,8 +9,5 @@ else
     wget -4qO - https://github.com/cloudreve/Cloudreve/releases/download/${VERSION}/cloudreve_${VERSION}_linux_amd64.tar.gz | tar -zxf - -C /workdir
 fi
 
-VERSION="$(curl -4 --retry 10 --retry-max-time 60 https://api.github.com/repos/cloudreve/Cloudreve/releases/latest | jq .tag_name | sed 's/\"//g')"
-wget -4qO - https://github.com/cloudreve/Cloudreve/releases/download/${VERSION}/cloudreve_${VERSION}_linux_amd64.tar.gz | tar -zxf - -C /usr/bin
-
 # Install Aria2
 wget -4qO - https://github.com/P3TERX/Aria2-Pro-Core/releases/download/1.36.0_2021.08.22/aria2-1.36.0-static-linux-amd64.tar.gz | tar -zxf - -C /usr/bin
